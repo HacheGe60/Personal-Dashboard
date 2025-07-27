818e7988472ed96fde47439655b7a52d938fe02d
lolodev <horaciodev1706@gmail.com>
2025-07-27 20:25:34 -0300

    Add author Info

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ async function getBackgroundImage() {
     const response = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=sailboat');
     const data = await response.json();
     bodyEl.style.backgroundImage = `url(${data.urls.regular})`;
+    bodyEl.innerHTML = `<p class='author'>Photo by ${data.user.name}</p>`;
 }
 
 getBackgroundImage();
