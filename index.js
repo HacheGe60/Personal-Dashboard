c9ef5ee41a01e29b038a12f3e982310b892f3fb1
lolodev <horaciodev1706@gmail.com>
2025-07-27 20:40:53 -0300

    Setup flexbox

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const bodyEl = document.querySelector('body');
+const authorEl = document.querySelector('#author');
 
 async function getBackgroundImage() {
     const response = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=sailboat');
     const data = await response.json();
     bodyEl.style.backgroundImage = `url(${data.urls.regular})`;
-    bodyEl.innerHTML = `<p class='author'>Photo by ${data.user.name}</p>`;
+    authorEl.textContent = `By ${data.user.name}`;
 }
 
 getBackgroundImage();
