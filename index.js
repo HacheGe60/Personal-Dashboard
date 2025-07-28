7caf90e5a020cd7af04044aa312800a1584cf332
lolodev <horaciodev1706@gmail.com>
2025-07-28 17:14:40 -0300

    Crypto - get prices

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bodyEl = document.querySelector('body');
 const authorEl = document.querySelector('#author');
 
 /* CRYPTO DATA */
-const cryptosEl = document.querySelector('#cryptos');
+const cryptoTopEl = document.querySelector('#crypto-top');
+const cryptoEndEl = document.querySelector('#crypto-end');
 
 /* GET BACKGROUND IMAGE */
 async function getBackgroundImage() {
@@ -24,19 +25,24 @@ getBackgroundImage();
 /* GET CURRENT DATA ON A CRYPTOCURRENCY */
 async function getCryptoData() {
     try {
-        const res = await fetch('https://api.coingecko.com/api/v3/coins/dogecoins');
+        const res = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
         if (!res.ok) {
             const statusText = res.statusText || "No description available";
             throw new Error(`Error ${res.status} - ${statusText}`);
         }
         const data = await res.json();
-        cryptosEl.innerHTML = `
-            <p class="crypto"><span class="crypto-image"><img src="${data.image.small}"></span> ${data.name}</p> `;
+        cryptoTopEl.innerHTML = `
+            <img src="${data.image.small}"><p>${data.name}</p>`;
+        cryptoEndEl.innerHTML = `
+        <p class="crypto">🎯 Price: $${data.market_data.current_price.ars}</p>
+        <p class="crypto">📈 High: $${data.market_data.high_24h.ars}</p>
+        <p class="crypto">📉 Low: $${data.market_data.low_24h.ars}</p>
+        `;
     } catch (error) {
-        cryptosEl.innerHTML = `
-            <p class="crypto">❌ ${error.message}</p>`;
+        cryptoTopEl.innerHTML = `
+            <p p class="crypto" >❌ ${error.message}</p> `;
     }
 
 }
 
-getCryptoData(); 
+getCryptoData();
